9521e0affb30d7fecff4591a11b0db81b746b254
Adrian Felipe Guerra <adrianguerra9703@gmail.com>
2026-09-09 19:37:39 -0500

    feat(job-agent): add computrabajo package

---
+++ job_agent/computrabajo/__init__.py
@@ -0,0 +1,5 @@
+"""Computrabajo integration for the local Job Agent application."""
+
+from job_agent.computrabajo.collector import ComputrabajoCollector, SearchRequest, SearchRunStatus
+
+__all__ = ["ComputrabajoCollector", "SearchRequest", "SearchRunStatus"]
