15f17e3e06b2b3bb77e8d86eefd20e2a7a4b6081
Adrian Felipe Guerra <adrianguerra9703@gmail.com>
2026-09-09 20:06:44 -0500

    feat(job-agent): export assisted application preparer

--- job_agent/computrabajo/__init__.py
+++ job_agent/computrabajo/__init__.py
@@ -1,5 +1,12 @@
 """Computrabajo integration for the local Job Agent application."""
 
+from job_agent.computrabajo.application import AssistedApplicationPreparer, PreparationStatus
 from job_agent.computrabajo.collector import ComputrabajoCollector, SearchRequest, SearchRunStatus
 
-__all__ = ["ComputrabajoCollector", "SearchRequest", "SearchRunStatus"]
+__all__ = [
+	"AssistedApplicationPreparer",
+	"ComputrabajoCollector",
+	"PreparationStatus",
+	"SearchRequest",
+	"SearchRunStatus",
+]
