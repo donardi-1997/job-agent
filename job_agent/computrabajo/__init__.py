b642495737576e26e970f881c5be86169858f868
Adrian Felipe Guerra <adrianguerra9703@gmail.com>
2026-09-09 20:30:21 -0500

    feat(job-agent): export batch apply runner

--- job_agent/computrabajo/__init__.py
+++ job_agent/computrabajo/__init__.py
@@ -1,10 +1,14 @@
 """Computrabajo integration for the local Job Agent application."""
 
 from job_agent.computrabajo.application import AssistedApplicationPreparer, PreparationStatus
+from job_agent.computrabajo.batch import BatchApplyRequest, BatchApplyRunner, BatchApplyStatus
 from job_agent.computrabajo.collector import ComputrabajoCollector, SearchRequest, SearchRunStatus
 
 __all__ = [
 	"AssistedApplicationPreparer",
+	"BatchApplyRequest",
+	"BatchApplyRunner",
+	"BatchApplyStatus",
 	"ComputrabajoCollector",
 	"PreparationStatus",
 	"SearchRequest",
